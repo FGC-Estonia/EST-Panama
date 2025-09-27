cbbde92994e2f1a875fb4a5372b420a9088793d6
Saarmets <65457878+saarmets@users.noreply.github.com>
2025-09-27 17:50:40 +0300

    ports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/HardwareConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/HardwareConstants.java
@@ -6,34 +6,18 @@ public class HardwareConstants {
     // Omnidrive motorports
 
     // Movement motors
-    public static final String LEFT_FRONT_MOTOR = "Motor_Port_0_CH";
-    public static final String RIGHT_FRONT_MOTOR = "Motor_Port_3_EH";
-    public static final String LEFT_BACK_MOTOR = "Motor_Port_3_CH";
-    public static final String RIGHT_BACK_MOTOR = "Motor_Port_0_EH";
+    public static final String LEFT_FRONT_MOTOR = "Motor_Port_0_EH";
+    public static final String RIGHT_FRONT_MOTOR = "Motor_Port_3_CH";
+    public static final String LEFT_BACK_MOTOR = "Motor_Port_3_EH";
+    public static final String RIGHT_BACK_MOTOR = "Motor_Port_0_CH";
     // Rope motors
-    public static final String ROPE_MOTOR = "Motor_Port_1_EH";
+    public static final String ROPE_MOTOR = "Motor_Port_1_CH";
     // Other gadgets' motors
-    public static final String WHEEL_MOTOR = "Motor_Port_2_CH";
+    public static final String WHEEL_MOTOR = "Motor_Port_2_EH";
 
-    public static final String FLAG_SERVO_MOTOR = "Servo_Port_0_CH";
-    public static final String BALL_PUSHER_MOTOR_LEFT = "Servo_Port_2_CH";
-    public static final String BALL_PUSHER_MOTOR_RIGHT = "Servo_Port_1_CH";
-    public static final String BALL_COLLECTOR_MOTOR = "Motor_Port_2_EH";
-
-
-
-    // Tankdrive motors
-
-    // Movement motors
-    public static final String TANK_LEFT_MOTOR = "Motor_Port_0_CH";
-    public static final String TANK_RIGHT_MOTOR = "Motor_Port_3_CH";
-    // Rope motors
-    public static final String TANK_ROPE_MOTOR = "Motor_Port_2_CH";
-    // Other gadgets' motors
-    public static final String TANK_BALL_COLLECTOR_MOTOR = "Motor_Port_3_CH";
-    public static final String TANK_LEFT_SLIDER_SERVO_MOTOR = "Servo_Port_3_EH";
-    public static final String TANK_RIGHT_SLIDER_SERVO_MOTOR = "Servo_Port_4_EH";
-    //public static final String TANK_WHEEL_SERVO_MOTOR = "Motor_Port_0_CH";
-    //public static final String TANK_BALL_PUSHER_MOTOR = "Motor_Port_1_CH";
+    public static final String FLAG_SERVO_MOTOR = "Servo_Port_0_EH";
+    public static final String BALL_PUSHER_MOTOR_LEFT = "Servo_Port_2_EH";
+    public static final String BALL_PUSHER_MOTOR_RIGHT = "Servo_Port_1_EH";
+    public static final String BALL_COLLECTOR_MOTOR = "Motor_Port_2_CH";
 
 }
